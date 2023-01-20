3aae64c8a91e5dc4b356d4d26cb2a9a8a149dd17
USMAN <101738023+skusman09@users.noreply.github.com>
2023-01-20 17:53:08 +0530

    Add files via upload

---
+++ Practicals/hello.java
@@ -0,0 +1,18 @@
+/**
+ * my
+ */
+class My {
+    public static void display() {
+        System.out.println("hello");
+    }
+}
+
+/**
+ * Innermy
+ */
+class hello {
+    public static void main(String[] args) {
+        System.out.println("my name is usman");
+        My.display();
+    }
+}

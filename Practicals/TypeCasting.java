f83d626c3547c29394eccdda0f9599e88d6125c8
USMAN <101738023+skusman09@users.noreply.github.com>
2023-01-23 18:07:29 +0530

    Update TypeCasting.java

--- Practicals/TypeCasting.java
+++ Practicals/TypeCasting.java
@@ -1,15 +1,16 @@
 class TypeCasting {
 
     public static void main(String args[]) {
-
+//     Widening Casting
         int a = 56;
         double b = a;
         System.out.println(a);
         System.out.println(b);
 
+        
+//         Narrowing Casting
         double x = 6767675;
         int y = (int)x;
-
         System.out.println(x);
         System.out.println(y);
     }

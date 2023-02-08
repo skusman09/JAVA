9ca73ad85a1dca65e6da273a528593c2dd7ce8b0
USMAN <101738023+skusman09@users.noreply.github.com>
2023-02-08 15:58:57 +0530

    Update Overloading.java

--- Practicals/Overloading.java
+++ Practicals/Overloading.java
@@ -1,21 +1,21 @@
 public class Overloading {
 
-    public int div(int x, int y) { // two int parameters
-        return (x / y);
+    public void div(int x, int y) { // two int parameters
+        System.out.println(x / y);
     }
 
-    public int div(int x, int y, int z) { // three int parameters
-        return (x / y / z);
+    public void div(int a, int b, int c) { // three int parameters
+        System.out.println(a / b / c);
     }
 
-    public double div(double x, double y) { // two double parameters
-        return (x / y);
+    public void div(double p, double q) { // two double parameters
+        System.out.println(p / q);
     }
 
     public static void main(String args[]) {
-        Overloading s = new Overloading();
-        System.out.println(s.div(30, 3));
-        System.out.println(s.div(80, 2, 4));
-        System.out.println(s.div(100, 25));
+        Overloading obj = new Overloading();
+        obj.div(30, 3);
+        obj.div(80, 2, 4);
+        obj.div(100, 25);
     }
 }

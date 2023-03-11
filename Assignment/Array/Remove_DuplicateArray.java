c5a73feefcbfd6825bb92af0994782cf504a39fa
USMAN <101738023+skusman09@users.noreply.github.com>
2023-03-12 00:55:07 +0530

    Update Remove_DuplicateArray.java

--- Assignment/Array/Remove_DuplicateArray.java
+++ Assignment/Array/Remove_DuplicateArray.java
@@ -1,10 +1,17 @@
+// Remove Duplicate Element From Array
+// Doubts Not Cleared
+// Incomplete
+
 public class Remove_DuplicateArray {
 
-  public static void main(String[] args) {
-    int arr[] = { 10, 10, 30, 30, 40, 40, 40, 60, 60, 60, 60 };
+	public static void main(String[] args) {
+		int arr[] = { 10, 10, 30, 30, 40, 40, 40, 60, 60, 60, 60 };
 
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i] + " ");
-    }
-  }
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+		System.out.println(" ");
+
+	}
 }

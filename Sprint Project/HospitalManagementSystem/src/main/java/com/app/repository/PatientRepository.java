6ab244d2215e663e58323f432d6c2887d56bbf34
USMAN <101738023+skusman09@users.noreply.github.com>
2023-06-14 12:29:05 +0530

    Add files via upload

---
+++ Sprint Project/HospitalManagementSystem/src/main/java/com/app/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+// @author: Mohammed Usman Shaikh
+
+package com.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.model.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+}

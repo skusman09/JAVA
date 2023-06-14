cdaa651b7fe4e4dc667d35fa99d2bb96f98250f6
USMAN <101738023+skusman09@users.noreply.github.com>
2023-06-14 12:33:58 +0530

    Add files via upload

---
+++ Sprint Project/Sprint-1 Project/HospitalManagementSystem/src/test/java/com/app/HospitalManagementSystem/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.HospitalManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
